e34b4504dd5b07b418f00cf388654b58bfd23286
Bankbuster777 <dallma300@gmail.com>
2023-10-13 13:57:03 +0900

    test

---
+++ sim_f1tenth_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dawgs/dawgs_car/sim_f1tenth_ws/src
+# Build directory: /home/dawgs/dawgs_car/sim_f1tenth_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("f1tenth_simulator")
---
+++ sim_f1tenth_ws/build/f1tenth_simulator/CMakeFiles/brake_emergency_v2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/dawgs/dawgs_car/sim_f1tenth_ws/devel/lib/f1tenth_simulator/brake_emergency_v2"
+  "/home/dawgs/dawgs_car/sim_f1tenth_ws/devel/lib/f1tenth_simulator/brake_emergency_v2.pdb"
+  "CMakeFiles/brake_emergency_v2.dir/node/brake_emergency_v2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/brake_emergency_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
